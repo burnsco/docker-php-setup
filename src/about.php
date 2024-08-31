cdc997bfafd6c115870f56ab4fccce3993c99efa
Corey Burns <coreyburns@outlook.com>
2024-08-31 12:57:22 -0400

    {1:-Updated Dependencies}

--- src/about.php
+++ src/about.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="./css/output.css">
-<div class="container mx-auto">
+
+<div class="container mx-auto p-10 border-x-black">
 	<h1 class="text-blue-300">About Page</h1>
 	<h2 class="italic">and other stuff</h2>
 </div>
